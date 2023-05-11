08c8f0b096f77f1101d0096ce8a9fffbdb95b274
N3wed1t <sippawit.c@kkumail.com>
2023-05-11 19:52:02 +0700

    Added stock_picker.rb

---
+++ stock_picker.rb
@@ -0,0 +1,20 @@
+def stock_picker(stock)
+  profit = {
+    init_profit: 0,
+    start_index: 0,
+    compare_index: 0,
+  }
+  stock.each_with_index do |value, index|
+    stock[index..-1].each_with_index do |check_value, check_index|
+      check_profit = check_value - value
+      if check_profit > profit[:init_profit]
+        profit[:init_profit] = check_profit
+        profit[:start_index] = index
+        profit[:compare_index] = check_index + index
+      end
+    end
+  end
+  return [profit[:start_index], profit[:compare_index]]
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
